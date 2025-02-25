ecec313df173ce625a68da68030d9fa05c525141
raufzer <a_zerkhef@estin.dz>
2025-02-25 21:15:58 +0100

    chore: update build script to remove dart-define option

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Build the Flutter web app with CSP compatibility
-flutter build web --csp --dart-define=FLUTTER_WEB_RENDERER=html
+flutter build web --csp 
 echo Flutter build completed with CSP compatibility.
