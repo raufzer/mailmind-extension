239f5cd1657c17958bc1717a0ee9b74bfba6b7f7
raufzer <a_zerkhef@estin.dz>
2025-02-25 23:38:48 +0100

    chore: add AssetsData class with constants for asset paths

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+class AssetsData {
+  static const mailMindLogo = 'assets/images/mailmind_logo.svg';
+  static const searchIcon = 'assets/icons/search_icon.svg';
+  static const settingsIcon = 'assets/icons/settings_icon.svg';
+  static const googleIcon = 'assets/icons/google_icon.svg';
+  static const logOuttIcon = 'assets/icons/log_out_icon.svg';
+
+
+}
