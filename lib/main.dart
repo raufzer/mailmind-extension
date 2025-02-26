5302096f10dc405e28d1310616a0b6f8f8aed93d
raufzer <a_zerkhef@estin.dz>
2025-02-26 23:48:44 +0100

    chore: refactor main app structure and integrate ScreenUtil for responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mail_mind_extension/core/config/app_routes.dart';
+import 'package:mail_mind_extension/core/theme/app_colors.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MailMindExtension());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MailMindExtension extends StatelessWidget {
+  const MailMindExtension({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return ScreenUtilInit(
+      designSize: const Size(600, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'MailMind',
+          routerConfig: AppRouters.router,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kTertiaryColor,
+            textTheme: GoogleFonts.interTextTheme(
+              ThemeData.dark().textTheme.apply(
+                bodyColor: kAccentColor,
+                displayColor: kAccentColor,
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          ),
+        );
+      },
     );
   }
 }
