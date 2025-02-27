65c1c83281fab97fe56bce19270a545bd4e1e238
raufzer <a_zerkhef@estin.dz>
2025-02-27 20:01:29 +0100

    feat: update WelcomeScreenBody to use HeadlineOne widget and new background image

--- lib/features/welcome/presentation/views/widgets/welcome_screen_body.dart
+++ lib/features/welcome/presentation/views/widgets/welcome_screen_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mail_mind_extension/core/shared/widgets/background_image_with_gradient.dart';
-import 'package:mail_mind_extension/features/welcome/presentation/views/widgets/welcome_title.dart';
+import 'package:mail_mind_extension/core/utils/app_assets.dart';
+import 'package:mail_mind_extension/core/shared/widgets/headline_one.dart';
 import 'package:mail_mind_extension/features/welcome/presentation/views/widgets/google_connect_button.dart';
 
 class WelcomeScreenBody extends StatelessWidget {
@@ -13,6 +14,7 @@ class WelcomeScreenBody extends StatelessWidget {
         children: <Widget>[
           const Positioned.fill(
             child: BackgroundImageWithGradient(
+              image: AssetsData.thumbImage3,
               height: 800,
               width: 600,
               opacityRight: 0.7,
@@ -23,7 +25,9 @@ class WelcomeScreenBody extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                const WelcomeTitle(),
+                const HeadlineOne(
+                  text: 'Welcome to MailMind Smarter Email, Faster Decisions.',
+                ),
                 const SizedBox(height: 20),
                 const GoogleConnectButton(),
               ],
