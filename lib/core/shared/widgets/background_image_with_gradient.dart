f94e75c409ac974a5c405a361cf656689a8842c8
raufzer <a_zerkhef@estin.dz>
2025-02-27 19:59:54 +0100

    feat: enhance BackgroundImageWithGradient widget with customizable image and border radius

--- lib/core/shared/widgets/background_image_with_gradient.dart
+++ lib/core/shared/widgets/background_image_with_gradient.dart
@@ -1,47 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:mail_mind_extension/core/utils/app_assets.dart';
 
 class BackgroundImageWithGradient extends StatelessWidget {
   final double opacityRight;
   final double opacityLeft;
   final double? height;
   final double? width;
+  final String? image;
+  final double radius;
 
   const BackgroundImageWithGradient({
     super.key,
-    this.opacityRight = 0.7,
-    this.opacityLeft = 0.3,
+    this.opacityRight = 0.0,
+    this.opacityLeft = 0.0,
     this.height,
     this.width,
+    this.image,
+    this.radius = 0.0,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      width: width,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            AssetsData.thumbImage3,
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-          ),
-
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.centerRight,
-                end: Alignment.centerLeft,
-                colors: [
-                  Colors.black.withOpacity(opacityRight),
-                  Colors.black.withOpacity(opacityLeft),
-                ],
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(radius),
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(image!, fit: BoxFit.cover, alignment: Alignment.center),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                    Colors.black.withOpacity(opacityRight),
+                    Colors.black.withOpacity(opacityLeft),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
