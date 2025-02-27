1bf093369ddf76a30a2b79f93278e11501077a7c
raufzer <a_zerkhef@estin.dz>
2025-02-27 20:00:39 +0100

    feat: add MainScreen widget to serve as the main entry point of the application

---
+++ lib/features/main/presentation/views/main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mail_mind_extension/features/main/presentation/views/widgets/main_screen_body.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: MainScreenBody());
+  }
+}
