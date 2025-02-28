c82e7068e07665403870517e0a2f1959b9fcb459
raufzer <a_zerkhef@estin.dz>
2025-02-28 09:46:17 +0100

    chore: uncomment routes for MainScreen and SettingsScreen in app routing configurationchore: uncomment routes for MainScreen and SettingsScreen in app routing configuration

--- lib/core/config/app_routes.dart
+++ lib/core/config/app_routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mail_mind_extension/features/main/presentation/views/main_screen.dart';
+import 'package:mail_mind_extension/features/settings/presentation/views/settings_screen.dart';
 import 'package:mail_mind_extension/features/welcome/presentation/views/welcome_screen.dart';
 
 abstract class AppRouters {
@@ -14,18 +16,18 @@ abstract class AppRouters {
           return const WelcomeScreen();
         },
       ),
-      // GoRoute(
-      //   path: kMainScreen,
-      //   builder: (BuildContext context, GoRouterState state) {
-      //     return const MainScreen();
-      //   },
-      // ),
-      // GoRoute(
-      //   path: kSettingsScreen,
-      //   builder: (BuildContext context, GoRouterState state) {
-      //     return const SettingsScreen();
-      //   },
-      // ),
+      GoRoute(
+        path: kMainScreen,
+        builder: (BuildContext context, GoRouterState state) {
+          return const MainScreen();
+        },
+      ),
+      GoRoute(
+        path: kSettingsScreen,
+        builder: (BuildContext context, GoRouterState state) {
+          return const SettingsScreen();
+        },
+      ),
     ],
   );
 }
